89f24ba36948111750690f3ba17319e858cc2fdd
Cherhazad <cherhazad.boustani@outlook.fr>
2024-07-31 22:14:14 +0200

    Ajout de routes sur un menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { AppComponent } from './app.component';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import {FormsModule} from "@angular/forms";
 import { MessageDetailsComponent } from './message-details/message-details.component';
+import {MyFirstService} from "./services/my-first.service";
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstCompComponent,
-    MessageDetailsComponent
+    MessageDetailsComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +24,8 @@ import { MessageDetailsComponent } from './message-details/message-details.compo
     FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    MyFirstService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/my-first.service.ts
+++ src/app/services/my-first.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable(
+  // {
+  //   providedIn: 'root' //si je retire cette propriété d'ici, il faut que je spécifie cette classe dans le providers de app.module.ts
+  // }
+)
 export class MyFirstService {
 
-  constructor() { }
+  messages: Array<any> = [];
+
+  constructor() {
+    this.init();
+  }
+
+  init(): void {
+    this.insert({
+      name: 'Aloe',
+      email: 'aloe@vera.com',
+      message: 'Hello World'
+    });
+    this.insert({
+      name: 'Aloca',
+      email: 'alocasia@vera.com',
+      message: 'Hello World'
+    });
+    this.insert({
+      name: 'Oxa',
+      email: 'oxalis@triangularis.com',
+      message: 'Hello World'
+    });
+  }
+
+  insert(message: { name: string, email: string, message: string }): void {
+    this.messages.push(message);
+  }
+
+  getAllMessages() {
+    return this.messages;
+  }
+
+  deleteMessage(index: number): void {
+    this.messages.splice(index, 1)
+  }
 }
